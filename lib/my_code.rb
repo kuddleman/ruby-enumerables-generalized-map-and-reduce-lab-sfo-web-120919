928851b4561a1905933b80f0572e2abdf0d7cfa6
Donald Lobree <edl1155@gmail.com>
2019-10-29 18:29:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,17 +10,6 @@ end
 
 
 
-# def map(array)
-#     new = []
-#     i = 0
-#     while i > array.length
-#       new.push(yield(array[i]))
-#       i += 1
-#     end
-#     new
-# end
-
-
 def reduce(array, starting_value = nil)
   if starting_value
     sum = starting_value
