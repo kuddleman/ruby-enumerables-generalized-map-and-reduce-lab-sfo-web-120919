4c013281a36bae8e64a3959931083d0dd773e424
Donald Lobree <edl1155@gmail.com>
2019-10-29 18:26:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,16 @@
 def map(array)
+  new = []
+  i = 0
+  while i > array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+
+
+def map(array)
     new = []
     i = 0
     while i > array.length
